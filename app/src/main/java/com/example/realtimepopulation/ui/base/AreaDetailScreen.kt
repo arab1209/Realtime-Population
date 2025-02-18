4152a12313b711f7bfd2f6e3b3c4f878cddaabe0
song-gichan <8345784@naver.com>
2025-02-18 14:07:50 +0900

    [feat] 가져온 날씨 환경 데이터 뷰에 전달

--- app/src/main/java/com/example/realtimepopulation/ui/base/AreaDetailScreen.kt
+++ app/src/main/java/com/example/realtimepopulation/ui/base/AreaDetailScreen.kt
@@ -1,6 +1,5 @@
 package com.example.realtimepopulation.ui.base
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -10,7 +9,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.material.Divider
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.CenterAlignedTopAppBar
@@ -27,14 +26,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
-import com.example.realtimepopulation.R
 import com.example.realtimepopulation.ui.main.viewmodel.MainViewModel
 import com.patrykandpatrick.vico.compose.axis.horizontal.rememberBottomAxis
 import com.patrykandpatrick.vico.compose.axis.vertical.rememberStartAxis
@@ -58,7 +55,10 @@ fun AreaDetailScreen(viewModel: MainViewModel = hiltViewModel(), navController: 
     LaunchedEffect(detailScreenData) {
         viewModel.getCongestMessage(detailScreenData.value!!.congestionMessage)
         viewModel.getChartData(detailScreenData.value!!)
+        viewModel.getWeatherStatus(detailScreenData.value!!.areaName)
     }
+
+    val weatherSttsData = viewModel.weatherSttsData.observeAsState()
     val congestMessageList = viewModel.congestMessage.collectAsState()
     val maxData = viewModel.chartMinMax.collectAsState()
     val chartData = viewModel.chartData.collectAsState()
@@ -77,117 +77,116 @@ fun AreaDetailScreen(viewModel: MainViewModel = hiltViewModel(), navController: 
                 .padding(paddingValues)
                 .fillMaxSize()
         ) {
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 15.dp, vertical = 15.dp)
-            ) {
-                Column() {
-                    Row(
-                        modifier = Modifier.fillMaxWidth(),
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        Text(
-                            "실시간 인구",
-                            fontSize = 22.sp,
-                            fontWeight = FontWeight.Bold,
-                            color = Color(0xff4c65a7),
-                        )
-                        Image(
-                            painter = painterResource(R.drawable.ic_right_button),
-                            contentDescription = "img",
-                            modifier = Modifier
-                                .width(35.dp)
-                                .height(35.dp)
-                        )
-                    }
-
-                    Row(
-                        verticalAlignment = Alignment.CenterVertically,
-                        horizontalArrangement = Arrangement.Center,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(top = 15.dp)
-                            .background(Color(0xfff7f7f7))
-                    ) {
-                        Text(
-                            "인구혼잡도", fontSize = 16.sp, textDecoration = TextDecoration.Underline
-                        )
-                        Text(
-                            "가 ", fontSize = 16.sp
-                        )
-                        Text(
-                            detailScreenData.value!!.congestionLevel,
-                            color = viewModel.calcAreaColor(detailScreenData.value!!.congestionLevel),
-                            fontSize = 24.sp,
-                            fontWeight = FontWeight.Bold,
-                            modifier = Modifier.padding(vertical = 5.dp)
-                        )
-                        Text(
-                            " 입니다", fontSize = 16.sp
-                        )
-                    }
-
-                    Box(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(top = 10.dp)
-                    ) {
-                        Column() {
-                            Text(
-                                text = "현재 실시간 인구 : ${detailScreenData.value?.minCount} ~ ${detailScreenData.value?.maxCount}명",
-                                fontSize = 16.sp,
-                                color = Color(0xff798fd2),
-                                fontWeight = FontWeight.Bold
-                            )
-                            congestMessageList.value.forEach {
-                                Text(
-                                    text = it,
-                                    color = Color(0xff626262),
-                                    fontSize = 12.sp,
-                                    modifier = Modifier.padding(top = 10.dp)
-                                )
-                            }
-                        }
-                    }
-
-                    Box(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(top = 10.dp)
-                    ) {
-                        Chart(
+            Column() {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 15.dp, vertical = 15.dp)
+                ) {
+                    Column() {
+                        DetailScreenTitleBox("실시간 인구")
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.Center,
                             modifier = Modifier
                                 .fillMaxWidth()
-                                .height(200.dp), chart = columnChart(
-                                columns = detailScreenData.value!!.forecasts.map { dataItem ->
-                                    lineComponent(
-                                        color = viewModel.calcAreaColor(dataItem.congestionLevel),
-                                        thickness = 15.dp,
-                                        shape = Shapes.cutCornerShape(
-                                            topRightPercent = 20,
-                                            topLeftPercent = 20
-                                        )
+                                .padding(top = 15.dp)
+                                .background(Color(0xfff7f7f7))
+                        ) {
+                            Text(
+                                "인구혼잡도", fontSize = 16.sp, textDecoration = TextDecoration.Underline
+                            )
+                            Text(
+                                "가 ", fontSize = 16.sp
+                            )
+                            Text(
+                                detailScreenData.value!!.congestionLevel,
+                                color = viewModel.calcAreaColor(detailScreenData.value!!.congestionLevel),
+                                fontSize = 24.sp,
+                                fontWeight = FontWeight.Bold,
+                                modifier = Modifier.padding(vertical = 5.dp)
+                            )
+                            Text(
+                                " 입니다", fontSize = 16.sp
+                            )
+                        }
+
+                        Box(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(top = 10.dp)
+                        ) {
+                            Column() {
+                                Text(
+                                    text = "현재 실시간 인구 : ${detailScreenData.value?.minCount} ~ ${detailScreenData.value?.maxCount}명",
+                                    fontSize = 16.sp,
+                                    color = Color(0xff798fd2),
+                                    fontWeight = FontWeight.Bold
+                                )
+                                congestMessageList.value.forEach {
+                                    Text(
+                                        text = it,
+                                        color = Color(0xff626262),
+                                        fontSize = 12.sp,
+                                        modifier = Modifier.padding(top = 10.dp)
                                     )
-                                },
-                                spacing = 30.dp,
-                                dataLabel = TextComponent.Builder().build(),
-                                axisValuesOverrider = AxisValuesOverrider.fixed(
-                                    minY = 0f, maxY = maxData.value.toFloat()
-                                )
-                            ), chartModelProducer = ChartEntryModelProducer(chartData.value.map {
-                                entryOf(
-                                    it.index, it.xData
-                                )
-                            }), startAxis = rememberStartAxis(
-                                itemPlacer = AxisItemPlacer.Vertical.default(maxItemCount = 6)
-                            ), bottomAxis = rememberBottomAxis(valueFormatter = { value, _ ->
-                                (chartData.value.map {
-                                    it.time
-                                }[value.toInt()])
-                            }), chartScrollState = rememberChartScrollState()
-                        )
+                                }
+                            }
+                        }
+
+                        Box(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(top = 10.dp)
+                        ) {
+                            Chart(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .height(200.dp),
+                                chart = columnChart(
+                                    columns = detailScreenData.value!!.forecasts.map { dataItem ->
+                                        lineComponent(
+                                            color = viewModel.calcAreaColor(dataItem.congestionLevel),
+                                            thickness = 15.dp,
+                                            shape = Shapes.cutCornerShape(
+                                                topRightPercent = 20, topLeftPercent = 20
+                                            )
+                                        )
+                                    },
+                                    spacing = 30.dp,
+                                    dataLabel = TextComponent.Builder().build(),
+                                    axisValuesOverrider = AxisValuesOverrider.fixed(
+                                        minY = 0f, maxY = maxData.value.toFloat()
+                                    )
+                                ),
+                                chartModelProducer = ChartEntryModelProducer(chartData.value.map {
+                                    entryOf(
+                                        it.index, it.xData
+                                    )
+                                }),
+                                startAxis = rememberStartAxis(
+                                    itemPlacer = AxisItemPlacer.Vertical.default(maxItemCount = 6)
+                                ),
+                                bottomAxis = rememberBottomAxis(valueFormatter = { value, _ ->
+                                    (chartData.value.map {
+                                        it.time
+                                    }[value.toInt()])
+                                }),
+                                chartScrollState = rememberChartScrollState()
+                            )
+                        }
                     }
+                }
+                Divider(
+                    color = Color(0xffe7e8ee),
+                    thickness = 5.dp
+                )
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 15.dp, vertical = 15.dp)
+                ) {
+                    DetailScreenTitleBox("실시간 상권")
                 }
             }
         }
---
+++ app/src/main/java/com/example/realtimepopulation/ui/base/DetailScreenTitleBox.kt
@@ -0,0 +1,39 @@
+package com.example.realtimepopulation.ui.base
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.realtimepopulation.R
+
+@Composable
+fun DetailScreenTitleBox (titleText: String) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = titleText,
+            fontSize = 22.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color(0xff4c65a7),
+        )
+        Image(
+            painter = painterResource(R.drawable.ic_right_button),
+            contentDescription = "img",
+            modifier = Modifier
+                .width(35.dp)
+                .height(35.dp)
+        )
+    }
+}
